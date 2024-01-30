a1239e3022f5c788b943760d8ea3d7ca60f17973
4lfach <alfarabibolatbek@gmail.com>
2024-01-30 17:30:51 +0800

    Add project files.

---
+++ LinkedIn_RazorPages/Pages/Movies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LinkedIn_RazorPages.Pages
+{
+    public class MoviesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
