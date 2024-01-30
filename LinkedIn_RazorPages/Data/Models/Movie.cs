fc4e5e5cffebd2a11c064287a1247e3c66dce981
4lfach <alfarabibolatbek@gmail.com>
2024-01-30 18:03:35 +0800

    One-way data binding in ASP.NET Razor Pages

---
+++ LinkedIn_RazorPages/Data/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkedIn_RazorPages.Data.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Rate { get; set; }
+        public string Description { get; set; }
+       
+    }
+}
