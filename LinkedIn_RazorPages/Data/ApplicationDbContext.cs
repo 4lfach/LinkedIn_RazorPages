37b9974312e68641495030957a46036531d88f5c
4lfach <alfarabibolatbek@gmail.com>
2024-01-30 19:18:44 +0800

    Initialised the first migration, updated the db and created the 'Movies' table

---
+++ LinkedIn_RazorPages/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LinkedIn_RazorPages.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkedIn_RazorPages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
