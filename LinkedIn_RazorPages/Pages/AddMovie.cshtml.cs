f3b53204fe49ac18b2bfe501e1f22c6f52e7de96
4lfach <alfarabibolatbek@gmail.com>
2024-01-30 18:41:50 +0800

    Model binding ASP.NET Razor Pages. Changed the bi-directional binding to model binding

--- LinkedIn_RazorPages/Pages/AddMovie.cshtml.cs
+++ LinkedIn_RazorPages/Pages/AddMovie.cshtml.cs
@@ -1,3 +1,4 @@
+using LinkedIn_RazorPages.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,24 @@ namespace LinkedIn_RazorPages.Pages
 {
     public class AddMovieModel : PageModel
     {
+        [BindProperty]
+        public Movie Movie { get; set; }
+
         public void OnGet()
         {
+            Movie = new Movie();
+            Movie.Title = "Wellcome";
+        }
+
+        public IActionResult OnPost()
+        {
+            string title = Movie.Title;
+            int rate = Movie.Rate;
+            string desc = Movie.Description;
+
+            Console.WriteLine($"{title} - {rate} - {desc}");
+
+            return Redirect("Movies");
         }
     }
 }
