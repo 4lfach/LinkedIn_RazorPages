973bb298c6ee9cf841b1d2dbce6fc1327d3353ed
4lfach <alfarabibolatbek@gmail.com>
2024-02-06 19:27:19 +0800

    ApplicationDbContext was removed from model files and instead services are used

---
+++ LinkedIn_RazorPages/Services/MoviesService.cs
@@ -0,0 +1,29 @@
+﻿using LinkedIn_RazorPages.Data;
+using LinkedIn_RazorPages.Data.Models;
+
+namespace LinkedIn_RazorPages.Services
+{
+    public class MoviesService : IMoviesService
+    {
+        private ApplicationDbContext _context;
+        public MoviesService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void AddMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+        }
+
+        public Movie GetMovie(int id)
+        {
+            return _context.Movies.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+    }
+}
